395baaf228cf66cbc3d0eb178f12bec6f5f472b7
Lengocphuong05 <132881217+Lengocphuong05@users.noreply.github.com>
2023-05-08 14:49:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    // Code PHP miêu tả sự "mặc kệ" khoảng trắng
+    $var1         =     15;
+    $var2 =
+    30;
+    $sum = $var1
+    +
+    $var2;
+ 
+    // "\n" để xuống dòng
+    echo $sum, "\n";
+ 
+    $sum1 = $var1 + $var2;
+    echo $sum1;
+?>
